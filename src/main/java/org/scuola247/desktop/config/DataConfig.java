28a7e7e44df6698ae2f9742699487b5c9ab4e3b0
Andrea Adami <fol@fulcro.net>
2014-02-06 19:08:49 +0100

    Some minor fix

--- src/main/java/org/scuola247/desktop/config/DataConfig.java
+++ src/main/java/org/scuola247/desktop/config/DataConfig.java
@@ -33,6 +33,68 @@ public class DataConfig {
 	public DataSource dataSource() throws NamingException {
 		Context ctx = new InitialContext();
 		return (DataSource) ctx.lookup("java:comp/env/jdbc/desktop");
+		
+		/*
+		
+		import java.sql.Connection;
+		  import java.sql.ResultSet;
+		  import java.sql.Statement;
+
+		  import org.apache.tomcat.jdbc.pool.DataSource;
+		  import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+		  public class SimplePOJOExample {
+
+		      public static void main(String[] args) throws Exception {
+		          PoolProperties p = new PoolProperties();
+		          p.setUrl("jdbc:mysql://localhost:3306/mysql");
+		          p.setDriverClassName("com.mysql.jdbc.Driver");
+		          p.setUsername("root");
+		          p.setPassword("password");
+		          p.setJmxEnabled(true);
+		          p.setTestWhileIdle(false);
+		          p.setTestOnBorrow(true);
+		          p.setValidationQuery("SELECT 1");
+		          p.setTestOnReturn(false);
+		          p.setValidationInterval(30000);
+		          p.setTimeBetweenEvictionRunsMillis(30000);
+		          p.setMaxActive(100);
+		          p.setInitialSize(10);
+		          p.setMaxWait(10000);
+		          p.setRemoveAbandonedTimeout(60);
+		          p.setMinEvictableIdleTimeMillis(30000);
+		          p.setMinIdle(10);
+		          p.setLogAbandoned(true);
+		          p.setRemoveAbandoned(true);
+		          p.setJdbcInterceptors(
+		            "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"+
+		            "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+		          DataSource datasource = new DataSource();
+		          datasource.setPoolProperties(p);
+
+		          Connection con = null;
+		          try {
+		            con = datasource.getConnection();
+		            Statement st = con.createStatement();
+		            ResultSet rs = st.executeQuery("select * from user");
+		            int cnt = 1;
+		            while (rs.next()) {
+		                System.out.println((cnt++)+". Host:" +rs.getString("Host")+
+		                  " User:"+rs.getString("User")+" Password:"+rs.getString("Password"));
+		            }
+		            rs.close();
+		            st.close();
+		          } finally {
+		            if (con!=null) try {con.close();}catch (Exception ignore) {}
+		          }
+		      }
+
+		  }
+		  
+		  
+		  */
+		  
+				
 	}
 
 	@Bean
